a556c52aebb937967fbc979337b018356aaa7ab2
aacp87 <153422170+aacp87@users.noreply.github.com>
2026-05-04 15:30:54 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Quitamos el "|| placeholder" para que si falla, nos diga por qué
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
+// Si las variables faltan, esto nos avisará en la consola
 if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("ERROR: No se han encontrado las variables de Supabase en Vercel")
+  console.error("Faltan las variables de entorno de Supabase")
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
